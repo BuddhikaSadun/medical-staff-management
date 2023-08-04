f2f06817a77227bab369e14d7030d1c957a5b710
buddhikaSadun917 <87796498+BuddhikaSadun@users.noreply.github.com>
2023-08-04 20:53:51 +0530

    modified user model class

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,27 +1,30 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema=new mongoose.Schema(
-    {
-    
-    email:{
-        type:String,
-        required:true,
-        unique: true,
-        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match:
+        /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
     },
 
-    password:{
-        type:String,
-        required:true
-
+    password: {
+      type: String,
+      required: true,
     },
-    
-},
-    {
+    position: {
+      type: String,
+      enum: ["Admin", "Staff"],
+      description: "Must be Admin or Staff",
+      required: false,
+    },
+  },
+  {
     versionKey: false,
     timestamps: true,
-    },
-
+  }
 );
 
-module.exports=mongoose.model('User',userSchema);
+module.exports = mongoose.model("User", userSchema);
