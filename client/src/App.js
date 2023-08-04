9249d24dbcc895542d224665cf34f196527fe37d
buddhikaSadun917 <87796498+BuddhikaSadun@users.noreply.github.com>
2023-08-04 20:49:49 +0530

    Modified app.js & index.js files

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 import Staff from "./components/Staff";
 import AddStaff from "./components/AddStaff";
 import UpdateStaff from "./components/UpdateStaff";
@@ -9,7 +11,9 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Staff />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Staff />} />
           <Route path="/add" element={<AddStaff />} />
           <Route path="/update/:id" element={<UpdateStaff />} />
         </Routes>
